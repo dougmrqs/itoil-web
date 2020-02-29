6d11d6e397c19be542a5832b0eb0cef0acc293b5
Douglas Marques <douglas.bmarques@hotmail.com>
2020-02-29 12:03:38 -0300

    Updates

---
+++ precache-manifest.f2d23b56552197f1d87617afc8a5ba83.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f857761af7e77227d9565056a6b200d2",
+    "url": "/itoil/index.html"
+  },
+  {
+    "revision": "ec3b62d1bc52b9b1c1f7",
+    "url": "/itoil/static/css/2.3fcd293c.chunk.css"
+  },
+  {
+    "revision": "77e57c80e0b00a5134ab",
+    "url": "/itoil/static/css/main.2817abd9.chunk.css"
+  },
+  {
+    "revision": "ec3b62d1bc52b9b1c1f7",
+    "url": "/itoil/static/js/2.7885befb.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/itoil/static/js/2.7885befb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77e57c80e0b00a5134ab",
+    "url": "/itoil/static/js/main.6e018e97.chunk.js"
+  },
+  {
+    "revision": "d528cdf6ed190623aa1d",
+    "url": "/itoil/static/js/runtime-main.65085757.js"
+  },
+  {
+    "revision": "f096d19df5e512a5d9cb3d9f50f9cef4",
+    "url": "/itoil/static/media/airplane.f096d19d.jpg"
+  },
+  {
+    "revision": "99968c1d165873651fe563fd42e0426f",
+    "url": "/itoil/static/media/ecosystem.99968c1d.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/itoil/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/itoil/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/itoil/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/itoil/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/itoil/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
